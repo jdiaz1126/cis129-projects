34d17439429d1429f970b025c933248abc1fabd7
jdiaz1126 <jdiaz98@mail.pima.edu>
2024-10-01 10:01:59 -0700

    Short story about me cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Author: Josh Diaz
+#My first Python lab in my CIS129 course
+#Prints a simple short story
+print('My name is Josh Diaz.')
+print('I am twenty years old.')
+print('I reside in Flagstaff.')
+print('My hobbies include snowboarding and working out.')
+print('I have three dogs.') 
